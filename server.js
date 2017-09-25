fc59f016376a03d1da9013516f190355a45d53bb
Nguyen Hoang Chuong <chuongnh.hcm@gmail.com>
2017-09-25 23:18:22 +0700

    simi

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 
+const Simsimi = require('simsimi');
+
+var simsimi = new Simsimi({
+    key: '5f9e6803-5335-4203-af19-176291421416'
+});
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -27,12 +33,16 @@ io.on('connection', function (socket) {
 
         // turn on
         if (msg.trim() == '1') {
-            turnon = true;
+            turnon = '1';
             socket.emit('message', 'Đã <b>bật</b> tự động trả lời tin nhắn.');
         }
         else if (msg.trim() == '0') {
-            turnon = false;
+            turnon = '0';
             socket.emit('message', 'Đã <b>tắt</b> tự động trả lời tin nhắn.');
+        }
+        else if (msg.trim() == '2') {
+            turnon = '0';
+            socket.emit('message', 'Đã <b>bật</b> simi trả lời tin nhắn.');
         }
         else {
             socket.emit('message', 'Đổi câu trả lời thành: ' + msg + ' thành công.');
@@ -48,7 +58,7 @@ http.listen(process.env.PORT || 8080, function () {
 var login = require("facebook-chat-api");
 const fs = require('fs');
 var answeredThreads = {};
-var turnon = false;
+var turnon = 0;
 
 // Create simple echo bot
 login({ email: "chuongnh.hcm@gmail.com", password: "&&**chuongnh.HCM" }, function callback(err, api) {
@@ -62,15 +72,35 @@ login({ email: "chuongnh.hcm@gmail.com", password: "&&**chuongnh.HCM" }, functio
 
         console.log(message.threadID);
 
-        if (turnon == false) return;
-        // nhắn lần đầu tiên
-        if (!answeredThreads.hasOwnProperty(message.threadID)) {
-            answeredThreads[message.threadID] = true;
-            api.sendMessage("Hiện tại mình đang đi ra ngoài, mình sẽ trả lời bạn ngay khi online.", message.threadID);
+        if (turnon == '0') { }
+        else if (turnon = '1') {
+            // nhắn lần đầu tiên
+            if (!answeredThreads.hasOwnProperty(message.threadID)) {
+                answeredThreads[message.threadID] = true;
+
+                simsimi.listen('Đồ ngu', function (err, msg) {
+                    if (err) return console.error(err);
+                    console.log('simsimi say:', msg);
+                });
+
+                api.sendMessage("Hiện tại mình đang đi ra ngoài, mình sẽ trả lời bạn ngay khi online.", message.threadID);
+            }
+            else {
+                //api.sendMessage("Nếu có việc gấp, vui lòng liên hệ mình qua SĐT: 0164 7931 390", message.threadID);
+                api.sendMessage("Hiện tại mình đang đi ra ngoài, mình sẽ trả lời bạn ngay khi online.", message.threadID);
+            }
         }
-        else {
-            //api.sendMessage("Nếu có việc gấp, vui lòng liên hệ mình qua SĐT: 0164 7931 390", message.threadID);
-            api.sendMessage("Hiện tại mình đang đi ra ngoài, mình sẽ trả lời bạn ngay khi online.", message.threadID);
+        else if (turnon == '2') {
+            simsimi.listen(message.body, function (err, rep) {
+                if (err) {
+                    //api.sendMessage('Hỏng hiểu', message.threadID);
+                    return console.error(err)
+                };
+                console.log('simsimi say:', rep);
+                api.sendMessage(rep, message.threadID);
+            });
+            return;
         }
+
     });
 });
